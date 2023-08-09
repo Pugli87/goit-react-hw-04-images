de04077f16f6c81d87c913c3384cd9a016f4535d
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-08-09 15:17:31 -0500

    Update ImageGalleryItem.jsx

--- src/components/imageFilter/imageGalleryItem/ImageGalleryItem.jsx
+++ src/components/imageFilter/imageGalleryItem/ImageGalleryItem.jsx
@@ -1,22 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
 import PropTypes from 'prop-types';
-
-const GalleryItem = styled.li`
-  cursor: pointer;
-`;
-
-const Image = styled.img`
-  width: 100%;
-  height: 260px;
-  border-radius: 15px;
-  &:hover {
-    transform: scale(1.03);
-    cursor: zoom-in;
-    box-shadow: 0px 2px 4px -1px #3f51b5, 0px 4px 5px 0px #3f51b5,
-      0px 1px 10px 0px #3f51b5;
-  }
-`;
+import { GalleryItem, Image } from '../styled-component/ImageGalleryItemStyles';
 
 const ImageGalleryItem = ({ imageUrl, alt, onClick }) => {
   return (
