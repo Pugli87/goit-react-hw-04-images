af15644f4e80ac544fceb4d1e401dfa8b63d24f6
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-08-09 15:17:36 -0500

    Update Modal.jsx

--- src/components/imageFilter/modal/Modal.jsx
+++ src/components/imageFilter/modal/Modal.jsx
@@ -1,29 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
+import { Overlay, ModalContent, Image } from '../styled-component/ModalStyles';
 import PropTypes from 'prop-types';
-
-const Overlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.8);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ModalContent = styled.div`
-  max-width: 80%;
-  max-height: 80%;
-`;
-
-const Image = styled.img`
-  border-radius: 15px;
-  width: 100%;
-  height: auto;
-`;
 
 const Modal = ({ imageUrl, alt, onClose }) => {
   return (
