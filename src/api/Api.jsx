4df7e422270c0668b021376896fad8f155e3885e
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-08-11 05:58:45 -0500

    Update Api.jsx

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -4,17 +4,15 @@ import axios from 'axios';
 const API_KEY = '37119471-67d3015a83a24c7694e1d7310';
 const IMAGES_PER_PAGE = 12;
 
-export const fetchArticlesWithQuery = async searchQuery => {
+export const fetchArticlesWithQuery = async (searchQuery, page) => {
   try {
     const response = await axios.get(
-      `https://pixabay.com/api/?q=${searchQuery}&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${IMAGES_PER_PAGE}`
+      `https://pixabay.com/api/?q=${searchQuery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${IMAGES_PER_PAGE}`
     );
     console.log('Response from API:', response.data);
-    return response.data.hits;
+    return response.data;
   } catch (error) {
     console.error('Error fetching images:', error);
     throw error;
   }
 };
-
-export default fetchArticlesWithQuery;
